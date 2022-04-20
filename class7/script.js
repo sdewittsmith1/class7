d127eed4e9771379583a4d70c63be0e4b27fbbd2
Rita Dewittsmith <sdewittsmith1@gmail.com>
2022-04-20 01:11:52 -0400

    class7

---
+++ class7/script.js
@@ -0,0 +1,12 @@
+
+function DogInfo(){
+    let getdog = prompt("Do you have a dog?");
+    if (getdog === "yes") {
+        document.write("I own a dog too!");
+    } else 
+    alert ("this may not be the page for you. Please get a dog and come back");
+    return; 
+  
+}
+
+DogInfo();
